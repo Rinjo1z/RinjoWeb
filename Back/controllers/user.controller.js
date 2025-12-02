8622c89c5b5db07a2e039ea6cd5caa6f6f999673
Juan <jpaezvillaveses@gmail.com>
2025-12-01 19:23:02 -0500

    correcion updateuser

--- Back/controllers/user.controller.js
+++ Back/controllers/user.controller.js
@@ -25,13 +25,13 @@ const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
         const updateData = req.body;
-        const updatedUser = await user.findByIdAndUpdate(id, updateData, { new: true });
+        const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true });
         if (!updatedUser) {
             return res.status(404).json({ ok: false, message: 'Usuario no encontrado' });
         }
         return res.status(200).json({ ok: true, data: 'usuario actualizado' });
     } catch (error) {
-        return res.status(500).json({ ok: false, message: 'Error actualizando al usuario' });
+        return res.status(500).json({ ok: false, message: 'Error actualizando al usuario', error: error.message });
     
     }
 };
